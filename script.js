6f13f7d631197f2ce1886698447fab0c95202c40
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-11-24 10:38:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+function change() {
+
+    let ele = document.getElementById("status");
+    
+    let newEle = document.createElement("h1");
+    newEle.innerText = "Entered Metaverse";
+
+    let btn = document.getElementById("enterBtn");
+    let parent = btn.parentElement;
+
+    parent.removeChild(ele);
+
+    parent.insertBefore(newEle , btn);
+
+}
+
+
+
